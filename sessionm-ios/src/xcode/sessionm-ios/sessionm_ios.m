649a378b835fac150bd25a545d3826ae0c8d8ff7
Antoine Lassauzay <alassauzay@ludia.com>
2014-01-30 17:55:21 -0500

    Initial iOS support
    --HG--
    branch : ios_support

---
+++ sessionm-ios/src/xcode/sessionm-ios/sessionm_ios.m
@@ -0,0 +1,140 @@
+/*
+ 
+ Copyright (c) 2012, DIVIJ KUMAR
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met: 
+ 
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer. 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution. 
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ The views and conclusions contained in the software and documentation are those
+ of the authors and should not be interpreted as representing official policies, 
+ either expressed or implied, of the FreeBSD Project.
+ 
+ 
+ */
+
+/*
+ * sessionm_ios.m
+ * sessionm-ios
+ *
+ * Created by Antoine Lassauzay on 1/30/2014.
+ * Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+ */
+
+#import "sessionm_ios.h"
+#import "SessionM.h"
+
+/* sessionm-iosExtInitializer()
+ * The extension initializer is called the first time the ActionScript side of the extension
+ * calls ExtensionContext.createExtensionContext() for any context.
+ *
+ * Please note: this should be same as the <initializer> specified in the extension.xml 
+ */
+void SessionMExtInitializer(void** extDataToSet, FREContextInitializer* ctxInitializerToSet, FREContextFinalizer* ctxFinalizerToSet)
+{
+    NSLog(@"Entering sessionm-iosExtInitializer()");
+
+    *extDataToSet = NULL;
+    *ctxInitializerToSet = &ContextInitializer;
+    *ctxFinalizerToSet = &ContextFinalizer;
+
+    NSLog(@"Exiting sessionm-iosExtInitializer()");
+}
+
+/* sessionm-iosExtFinalizer()
+ * The extension finalizer is called when the runtime unloads the extension. However, it may not always called.
+ *
+ * Please note: this should be same as the <finalizer> specified in the extension.xml 
+ */
+void SessionMExtFinalizer(void* extData)
+{
+    NSLog(@"Entering sessionm-iosExtFinalizer()");
+
+    // Nothing to clean up.
+    NSLog(@"Exiting sessionm-iosExtFinalizer()");
+    return;
+}
+
+/* ContextInitializer()
+ * The context initializer is called when the runtime creates the extension context instance.
+ */
+void ContextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx, uint32_t* numFunctionsToTest, const FRENamedFunction** functionsToSet)
+{
+    NSLog(@"Entering ContextInitializer()");
+    
+    /* The following code describes the functions that are exposed by this native extension to the ActionScript code.
+     */
+    static FRENamedFunction func[] = 
+    {
+        MAP_FUNCTION(startSession, NULL),
+    };
+    
+    *numFunctionsToTest = sizeof(func) / sizeof(FRENamedFunction);
+    *functionsToSet = func;
+    
+    NSLog(@"Exiting ContextInitializer()");
+}
+
+/* ContextFinalizer()
+ * The context finalizer is called when the extension's ActionScript code
+ * calls the ExtensionContext instance's dispose() method.
+ * If the AIR runtime garbage collector disposes of the ExtensionContext instance, the runtime also calls ContextFinalizer().
+ */
+void ContextFinalizer(FREContext ctx) 
+{
+    NSLog(@"Entering ContextFinalizer()");
+
+    // Nothing to clean up.
+    NSLog(@"Exiting ContextFinalizer()");
+    return;
+}
+
+
+/* This is a TEST function that is being included as part of this template. 
+ *
+ * Users of this template are expected to change this and add similar functions 
+ * to be able to call the native functions in the ANE from their ActionScript code
+ */
+ANE_FUNCTION(startSession)
+{
+    NSLog(@"Entering startSession()");
+    
+    if(argc > 0)
+    {
+        uint32_t len;
+        const uint8_t* token = 0;
+        if(FRE_OK == FREGetObjectAsUTF8(argv[0], &len, &token))
+        {
+            NSLog(@"Received appID %s", token);
+            SMStart([NSString stringWithUTF8String:(char *)token]);
+        }
+        else
+        {
+            NSLog(@"Error when reading appID");
+        }
+    }
+    else
+    {
+        NSLog(@"Missing appID argument");
+    }
+    
+	return NULL;
+}
+
